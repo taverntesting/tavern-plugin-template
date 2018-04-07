1de50e6bb3e681687a6eb62b7d0d2587510221ca
Ben Howes <ben@zoetrope.io>
2018-04-07 16:01:59 +0100

    Added all the boilerplate, though still contains most of the flask code at the moment

---
+++ tavern_<plugin>/response.py
@@ -0,0 +1,23 @@
+import logging
+
+from tavern._plugins.rest.response import RestResponse
+
+logger = logging.getLogger(__name__)
+
+
+class FlaskResponse(RestResponse):
+    """Flask response verifier"""
+
+    def verify(self, response):
+        """Wrap the Flask response into a Requests response and call the
+        verifier for that. See docs for RestResponse for what this does and
+        returns
+        """
+
+        from requests import Response
+        wrapped_response = Response()
+        wrapped_response.headers = response.headers
+        wrapped_response.status_code = response._status_code
+        wrapped_response._content = response.get_data()
+
+        return super(FlaskResponse, self).verify(wrapped_response)
